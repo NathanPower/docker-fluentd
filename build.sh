3341a1963e86ad3934616160e244e0d653dbd54e
Nathan Power <nathanpower78@gmail.com>
2015-04-11 15:50:09 +0100

    Get hostname from environment variable passed to 'docker run'

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd /home/nathan/workspaces/docker-fluentd
 docker rm -f `docker ps -a -q`
 docker build -t="nathanpower/docker-fluentd" .
-container_id=$(docker run  --name docker_fluentd -d -v /var/lib/docker/containers:/var/lib/docker/containers -v /var/run/docker.sock:/var/run/docker.sock  nathanpower/docker-fluentd)
+docker run -e HOST="$(uname -n)" --name docker_fluentd -d -v /var/lib/docker/containers:/var/lib/docker/containers -v /var/run/docker.sock:/var/run/docker.sock nathanpower/docker-fluentd 
 docker run --name daemon_dave -d ubuntu /bin/sh -c "while true; do echo hello world; sleep 1; done"
 sleep 10
 docker stop daemon_dave
